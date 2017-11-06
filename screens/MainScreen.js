995c962abfaa8e3fc86575dd2e8d5bfd802a452a
aabreu1 <a.abreu.1990@gmail.com>
2017-11-05 22:15:36 -0400

    Reducers & Actions

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
-import { View, Platform, Text } from 'react-native';
+import { View, Platform, Image } from 'react-native';
+import Expo from 'expo';
+import icon from '../assets/icons/pure-icon.png';
 import { STATUS_BAR_HEIGHT } from '../constants';
+
+
+const chacheImages = images => images.map(image => {
+    if (typeof image === 'string') return Image.prefetch(image);
+    return Expo.Asset.fromModule(image).downloadAsync();
+  });
 
 class MainScreen extends Component {
     static navigationOptions = () => ({
@@ -13,8 +21,26 @@ class MainScreen extends Component {
           marginTop: Platform.OS === 'android' ? STATUS_BAR_HEIGHT : 0,
           color: 'white'
         },
-        headerLeft: <View><Text>I</Text></View>
+        headerLeft: ( 
+          <Image
+            source={icon}
+            style={style.imageStyle}
+          />
+        )
     });
+
+    state = {
+      appIsReady: false
+    }
+
+    ComponentWillMount() {
+      this._loadAssetsAsync();
+    }
+    async _loadAssetsAsync() {
+      const imageAssets = chacheImages([images])
+      await Promise.all([...imageAssets]);
+      this.setState({ appIsReady: true });
+    }
     render() {
         return (
             <View style={{ flex: 1, backgroundColor: '#ddd' }}>
@@ -24,7 +50,15 @@ class MainScreen extends Component {
             </View>
           );
     }
-
 }
+
+const style = {
+  imageStyle: {
+    marginTop: 20,
+    marginLeft: 10,
+    width: 40,
+    height: 40
+  }
+};
 
 export default MainScreen;
